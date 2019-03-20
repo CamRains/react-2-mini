33e49ee0d56c81f49c0dcd361954c0c01f03f154
cam rains <csr66@nau.edu>
2019-03-20 14:04:21 -0700

    finished lecture react 2 mini on 3-20-19 week3 day3

--- src/App.js
+++ src/App.js
@@ -1,37 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Components
-import EditToggle from './components/EditToggle';
-import ColorChanger from './components/ColorChanger';
-import SizeChanger from './components/SizeChanger';
-import FamilyChanger from './components/FamilyChanger';
-import TextContainer from './components/TextContainer';
+import EditToggle from "./components/EditToggle";
+import ColorChanger from "./components/ColorChanger";
+import SizeChanger from "./components/SizeChanger";
+import FamilyChanger from "./components/FamilyChanger";
+import TextContainer from "./components/TextContainer";
 
 class App extends Component {
   // constructor
+  constructor() {
+    super();
+    this.state = {
+      fontColor: "black",
+      fontSize: 12,
+      fontFamily: "monospace",
+      allowEdit: "true"
+    };
+    this.updateColor = this.updateColor.bind(this);
+    this.updateSize = this.updateSize.bind(this);
+    this.updateFamily = this.updateFamily.bind(this);
+    this.updateEditStatus = this.updateEditStatus.bind(this);
+  }
 
   // updateColor
+  // our function wil take in a value ad call setState to update state in Ap.js with the values being passed in
 
+  updateColor(val) {
+    // set stage will mere the object that is passed in with the curent state, it will then call a render when the state has finished updating
+    this.setState({ fontColor: val });
+  }
   // updateSize
 
-  // updateFamily
+  updateSize(val) {
+    this.setState({ fontSize: val });
+  }
 
+  // updateFamily
+  updateFamily(val) {
+    this.setState({ fontFamily: val });
+  }
   // updateEditStatus
+  updateEditStatus(val) {
+    this.setState({ allowEdit: val });
+  }
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <div className="headerBar">
-          { /* Render EditToggle */ }
-          { /* Render ColorChanger */ }
-          { /* Render SizeChanger */ }
-          { /* Render FamilyChanger */ }
+          {/* Render EditToggle */}
+          <EditToggle update={this.updateEditStatus} />
+          {/* Render ColorChanger */}
+          <ColorChanger
+            update={this.updateColor}
+            allowEdit={this.state.allowEdit}
+          />
+          {/* Render SizeChanger */}
+          <SizeChanger
+            update={this.updateSize}
+            allowEdit={this.state.allowEdit}
+          />
+          {/* Render FamilyChanger */}
+          <FamilyChanger
+            update={this.updateFamily}
+            allowEdit={this.state.allowEdit}
+          />
         </div>
         <div className="textArea">
-          { /* Render TextContainer */ }
+          <TextContainer
+            fontSize={this.state.fontSize}
+            fontColor={this.state.fontColor}
+            fontFamily={this.state.fontFamily}
+          />
+
+          {/* Render TextContainer */}
         </div>
       </div>
-    )
+    );
   }
 }
 
